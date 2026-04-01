9d2739558c7b08599d1c8966145c3211c165837d
Vedant Tewari <74680679+xevor11@users.noreply.github.com>
2026-04-01 13:35:31 +0530

    Delete Parser/ParserTerminalNode.hs

--- Parser/ParserTerminalNode.hs
+++
@@ -1,9 +0,0 @@
-module Parser.TerminalNode where
-
-type Identifier = String
-type Type = String
-
-toSymbol :: String -> Symbol
-toSymbol = id
-toExpr :: String -> Token
-toExpr str = STRING_LIT { getStrVal = str, getPosition = Position 0 0 }
